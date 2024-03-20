6a0fbf48f9951d3351c426acacf597292eec0fae
Nhan <nhan@Nhans-iMac-Pro.local>
2024-03-21 01:08:47 +0700

    create product page /pages/product.html

---
+++ src/script/product.js
@@ -0,0 +1,3 @@
+function navigate () {
+  window.location.href="/src/pages/product.html";
+}
---
+++ src/script/tabs.js
@@ -0,0 +1,9 @@
+import { Tablist } from "https://cdn.jsdelivr.net/npm/jolty/dist/jolty.esm.min.js"
+
+// Tablist.data("tabs", (tablistElem) => {
+//   return {
+//     data: "ui-tabs",
+//     tabpanel: tablistElem.nextElementSibling.children,
+//   };
+// });
+Tablist.initAll();
