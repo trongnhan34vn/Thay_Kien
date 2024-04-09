49f908179d1621936aa98e47bc4bdd4f08e8eae0
Nhantic <trongnhan34vn@gmail.com>
2024-04-09 21:29:17 +0700

    update

--- src/script/Partners.js
+++ src/script/Partners.js
@@ -14,13 +14,28 @@ const partners = [
   {
     img: '/src/assets/images/logo-amaron.04de7f48.png'
   },
+  {
+    img: '/src/assets/images/logo-amaron.04de7f48.png'
+  },
+  {
+    img: '/src/assets/images/logo-amaron.04de7f48.png'
+  },
+  {
+    img: '/src/assets/images/logo-amaron.04de7f48.png'
+  },
+  {
+    img: '/src/assets/images/logo-amaron.04de7f48.png'
+  },
 ]
 
-const renderPartner = (partner, index) => {
-  return `<li class="slider-item w-11/12 tablet:w-full">
-  <img class="w-full object-contain block border-[1px] border-[#895609]" src="${partner.img}" alt="">
-</li>`;
-}
+// const renderPartner = (partner, index) => {
+//   //   return `<li class="slider-item w-11/12 tablet:w-full">
+//   //   <img class="w-full object-contain block border-[1px] border-[#895609]" src="${partner.img}" alt="">
+//   // </li>`;
+//   return `<div class="slide w-11/12 tablet:w-full">
+// <img class="mx-10 object-contain block border-[1px] border-[#895609]" src="${partner.img}" height="100" width="250" alt="" />
+// </div>`
+// }
 
 const renderPartners = () => {
   let render = "";
